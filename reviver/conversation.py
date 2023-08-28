ec002fb02590ab61ef3ec15f9e8e9646903ba111
mprib <prible@gmail.com>
2023-08-28 14:47:54 -0500

    conversation and message with basic tests

--- reviver/conversation.py
+++ reviver/conversation.py
@@ -1,14 +1,14 @@
 
 import reviver.logger
 logger = reviver.logger.get(__name__)
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from datetime import datetime
 
 @dataclass(frozen=True, slots=True)
 class Message:
     role: str
     content: str
-    time: datetime
+    time: datetime = datetime.now()
 
     @property
     def token_size(self):
@@ -16,12 +16,29 @@ class Message:
         Token size is a generalization based on the rule that 1 token ~= 4 characters:
         https://help.openai.com/en/articles/4936856-what-are-tokens-and-how-to-count-them
         """
-        len(self.content)/4
-        
+        return len(self.content)/4
+
     @property
     def chat_bubble(self):
        return {"role":self.role, "content":self.content} 
  
 @dataclass(frozen=False, slots=True)
 class Conversation:
-    messages: list[Message]
+    messages: list = field(default_factory=list[Message])
+
+    def add_message(self, msg:Message)->None:
+        self.messages.append(msg)
+        
+    def to_string_list(self):
+        
+        msgs_for_load = [] 
+        for msg in self.messages:
+            msgs_for_load.append(msg.chat_bubble)
+        return msgs_for_load
+
+    @property
+    def token_size(self):
+        size = 0
+        for msg in self.messages:
+            size+=msg.token_size
+        return size
--- tests/test_conversation.py
+++ tests/test_conversation.py
@@ -1,6 +1,42 @@
-import reviver.logger 
+import reviver.logger
+
 logger = reviver.logger.get(__name__)
+from reviver.conversation import Message, Conversation
 
 
-def test_testing():
-    assert(True)
+def test_conversation():
+    msg1 = Message(role="user", content="This is a test")
+    msg2 = Message(role="assistant", content="I'm just here to help.")
+
+    convo = Conversation()
+
+    convo.add_message(msg1)
+    convo.add_message(msg2)
+
+    logger.info(f"conversation list is {convo.to_string_list()}")
+    target_list = [
+        {"role": "user", "content": "This is a test"},
+        {"role": "assistant", "content": "I'm just here to help."},
+    ]
+    assert convo.to_string_list() == target_list
+
+def test_token_size():
+    msg1 = Message(role="user", content="This is a test")
+    msg2 = Message(role="assistant", content="This is a test")
+
+    logger.info(f"test message size is {msg1.token_size}")
+    assert(msg1.token_size == msg2.token_size)
+    
+    
+    convo = Conversation()
+
+    convo.add_message(msg1)
+    convo.add_message(msg2)
+
+    assert(convo.token_size == msg1.token_size+msg2.token_size)
+    logger.info(f"conversation size is {convo.token_size}")
+
+
+if __name__ == "__main__":
+    test_conversation()
+    test_token_size()
