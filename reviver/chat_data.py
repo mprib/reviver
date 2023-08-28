033ebc68b8e1cec1c5077c3af6c36d075c406af5
mprib <prible@gmail.com>
2023-08-28 14:03:28 -0500

    general basic idea of data storage

---
+++ reviver/chat_data.py
@@ -0,0 +1,27 @@
+
+import reviver.logger
+from dataclasses import dataclass
+from datetime import datetime
+logger = reviver.logger.get(__name__)
+
+@dataclass(frozen=True, slots=True)
+class Message:
+    role: str
+    content: str
+    time: datetime
+
+    @property
+    def token_size(self):
+        """
+        Token size is a generalization based on the rule that 1 token ~= 4 characters:
+        https://help.openai.com/en/articles/4936856-what-are-tokens-and-how-to-count-them
+        """
+        len(self.content)/4
+        
+    @property
+    def chat_bubble(self):
+       return {"role":self.role, "content":self.content} 
+ 
+@dataclass(frozen=False, slots=True)
+class Conversation:
+    messages: list[Message]
