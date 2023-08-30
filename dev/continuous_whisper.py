4e618c95652972d21fd5eb1cafc6271f644028b2
mprib <prible@gmail.com>
2023-08-30 07:48:29 -0500

    record chunk of speech then transcribe

--- dev/continuous_whisper.py
+++ dev/continuous_whisper.py
@@ -1,9 +1,64 @@
+"""
+Don't bother with trying to do fancy things to detect the beginning and end of speech. Just let the user
+control that with tapping a key. It will be ok. I think a guiding light of this whole thing will be the idea
+that we can get humans to adapt to working with LLMs quickly. So let's build some workflows that may involve 
+building new muscle memory, but that can make for robust interactions with LLMs. 
+
+
+"""
+import reviver.logger
+logger = reviver.logger.get(__name__)
 import openai
 import base64
 from keys import WHISPER_API_KEY
+import pyaudio
+import wave
+from pathlib import Path
+from threading import Thread, Event
+import time
 
 # Substitute 'your-openai-api-key' with your actual OpenAI API key
 openai.api_key = WHISPER_API_KEY
+
+# Define some constants
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+CHUNK = 1024
+RECORD_SECONDS = 5  # Change this to the duration you want
+WAVE_OUTPUT_FILENAME = "dev/file.wav"
+
+def record_chunk(stop_event:Event, destination_file:Path):
+    # Initialize PyAudio
+    audio = pyaudio.PyAudio()
+    # Start recording
+    stream = audio.open(format=FORMAT, channels=CHANNELS,
+                    rate=RATE, input=True,
+                    frames_per_buffer=CHUNK)
+    frames = []
+    print('Recording...')
+    while not stop_event.is_set():
+    # for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+
+    print('Finished recording')
+
+    # Stop recording
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+
+    # Save as a WAV file
+    wf = wave.open(str(destination_file), 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(audio.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+    
+    logger.info('Done. Your recording is saved as ' + str(destination_file)+ '.')
+    stop.clear()
 
 def transcribe(file_path):
     with open(file_path, "rb") as f:
@@ -15,50 +70,23 @@ def transcribe(file_path):
     )   
 
     print(transcript)
+
 # Substitute 'file.wav' with your actual .wav file path
-file_path = 'dev/file.wav'
-print(transcribe(file_path))
+if __name__=="__main__":
+    from reviver import ROOT
+
+    stop = Event()
+    stop.clear() # just to be explicit
 
 
-import pyaudio
-import wave
+    temp_wav = Path(ROOT, "dev", "temp.wav")
 
-# Define some constants
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 44100
-CHUNK = 1024
-RECORD_SECONDS = 5  # Change this to the duration you want
-WAVE_OUTPUT_FILENAME = "dev/file.wav"
+    thread = Thread(target=record_chunk, args=[stop, temp_wav], daemon=True)
+    thread.start()
+    wait = input("Press Enter to Stop")
+    stop.set()
 
-# Initialize PyAudio
-audio = pyaudio.PyAudio()
-
-# Start recording
-stream = audio.open(format=FORMAT, channels=CHANNELS,
-                rate=RATE, input=True,
-                frames_per_buffer=CHUNK)
-frames = []
-
-print('Recording...')
-
-for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-    data = stream.read(CHUNK)
-    frames.append(data)
-
-print('Finished recording')
-
-# Stop recording
-stream.stop_stream()
-stream.close()
-audio.terminate()
-
-# Save as a WAV file
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(audio.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-wf.writeframes(b''.join(frames))
-wf.close()
-
-print('Done. Your recording is saved as ' + WAVE_OUTPUT_FILENAME + '.')
+    while stop.is_set():
+        time.sleep(0.1)
+    
+    print(transcribe(temp_wav))
