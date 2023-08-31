1a00c6f707157fbe337f18e03a9e918fe324093c
mprib <prible@gmail.com>
2023-08-31 18:11:04 -0500

    very basic bot storage working

--- reviver/archiver.py
+++ reviver/archiver.py
@@ -15,11 +15,13 @@ from pathlib import Path
 class Archiver:
     def __init__(self, reviver_dir: Path) -> None:
         self.reviver_dir = reviver_dir
+        self.user_id = 1 # current way I'm thinking about it, but who knows?
         self.set_db_connection()
 
         self.create_tables() # won't do anyything if they already exist
         
     def set_db_connection(self)->sqlite3.Connection:
+        self.reviver_dir.mkdir(parents=True, exist_ok=True)
     
         target_db = Path(self.reviver_dir, "reviver.db")
         try:
@@ -31,7 +33,6 @@ class Archiver:
 
     def create_tables(self):
         cursor = self.conn.cursor()
-
         cursor.execute("""
         CREATE TABLE IF NOT EXISTS user (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -44,7 +45,14 @@ class Archiver:
         CREATE TABLE IF NOT EXISTS bots (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL,
-            user_id INTEGER,
+            model TEXT NOT NULL,
+            system_prompt TEXT NOT NULL,
+            max_tokens INT NOT NULL,
+            temperature FLOAT NOT NULL,
+            top_p FLOAT NOT NULL,
+            frequency_penalty FLOAT NOT NULL,
+            presence_penalty FLOAT NOT NULL,
+            user_id INTEGER NOT NULL,
             FOREIGN KEY(user_id) REFERENCES users(id)
         );
         """)
@@ -72,9 +80,25 @@ class Archiver:
         """)
 
 
-    def store_bot(self,bot:Bot)->None:
-        pass
-    
+    def store_bot(self, bot: Bot) -> None:
+        cursor = self.conn.cursor()
+        bot_data = asdict(bot)
+        bot_data["user_id"] = self.user_id  # Add user_id to the dictionary
+        placeholders = {}
+        for key, value in bot_data.items():
+            placeholders[key] = value
+             
+        sql = """
+            INSERT INTO bots 
+            (name, model, system_prompt, max_tokens, temperature, top_p, frequency_penalty, presence_penalty, user_id) 
+            VALUES 
+            (:name, :model, :system_prompt, :max_tokens, :temperature, :top_p, :frequency_penalty, :presence_penalty, :user_id)
+            """
+            
+            
+        cursor.execute(sql, placeholders)
+        self.conn.commit() 
+ 
     def get_bot(self, bot_id:int)->Bot:
         pass
     
@@ -91,3 +115,9 @@ class Archiver:
     
     def get__conversation(self,convo_id)->Conversation:
         pass
+
+if __name__ == "__main__":
+    test_bot = Bot(name = 'test_bot', model="llama70b")
+    arch = Archiver(Path(ROOT, "tests", "working_delete"))
+
+    arch.store_bot(test_bot)
