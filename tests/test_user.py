9055e7a894c005840430ff887d5b2e9acc82f3cc
mprib <prible@gmail.com>
2023-08-31 11:15:20 -0500

    basic user test

---
+++ tests/test_user.py
@@ -0,0 +1,32 @@
+
+
+#%%
+import reviver.logger
+
+logger = reviver.logger.get(__name__)
+from reviver.conversation import Message, Conversation
+from reviver.archiver import Archiver
+from pathlib import Path
+from reviver import ROOT
+from reviver.bot import Bot
+from reviver.user import User
+from reviver.helper import delete_directory_contents
+
+
+def test_user_creation_and_load():
+    
+    user = User("Test_user")
+
+    bot1 = Bot("New bot", model= "meta-llama/llama-2-70b-chat")
+    bot2 = Bot("Second bot", model= "meta-llama/llama-2-70b-chat")
+    bot3 = Bot("Third bot", model= "meta-llama/llama-2-70b-chat")
+
+    user.add_bot(bot1)
+    user.add_bot(bot2)
+    user.add_bot(bot3)
+
+    assert(user.bot_count == 3)
+
+
+if __name__== "__main__":
+    test_user_creation_and_load()
