e1010f90b8a1b9bebd31036ed2fa91caec1c43e2
mprib <prible@gmail.com>
2023-08-31 17:05:30 -0500

    all of this is broken but needs a dedicated branch

--- reviver/archiver.py
+++ reviver/archiver.py
@@ -6,16 +6,23 @@ logger = reviver.logger.get(__name__)
 from pathlib import Path
 from reviver.conversation import Conversation, Message
 from reviver.bot import Bot
-
+from reviver.user import User
 
 class Archiver:
     def __init__(self, user_dir: Path) -> None:
         self.profile_dir = user_dir
+        self.user_toml = Path(self.profile_dir, "user.toml")
 
+        #make sure that there is a place to store data
+        if not self.user_toml.parent.exists():
+            self.user_toml.parent.mkdir(parents=True, exist_ok=True)
+        
     def store_conversation(self, convo: Conversation) -> None:
+        self.store_user(convo.user)
+        
         toml_data = {}
         toml_data["bot"] = asdict(convo.bot)
-
+        
         messages = get_messages_for_toml(convo)
         toml_data["messages"] = messages
         logger.info(messages)
@@ -29,15 +36,38 @@ class Archiver:
         with open(target_path, "w") as f:
             rtoml.dump(toml_data, f)
 
-    def get_bot(self, bot_name: str, title: str) -> Bot:
-        target_dir = Path(self.profile_dir, bot_name)
-        target_path = Path(target_dir, f"{title}.toml")
+    def store_user(self,user:User)-> None:
+        with open(self.user_toml, "w") as f:
+            
+            # preliminary data will have bots as dictionary of Bot
+            preliminary_data = asdict(user)
 
-        conversation_data = rtoml.load(target_path)
-        bot_data = conversation_data["bot"]
-        bot = Bot(**bot_data)
+            for key, value in preliminary_data.items():
+                if 
+            
+            rtoml.dump(asdict(user), self.user_toml)
 
-        return bot
+    def get_user(self)->User:
+        
+        user_data = rtoml.load(self.user_toml)
+        user = User(**user_data)
+        return user
+
+    def save_bot(self, bot:Bot)->None:
+        
+        pass 
+        
+    def get_bot(self, bot_name: str) -> Bot:
+
+        user_data = rtoml.load(self.user_toml)
+        bots_data = user_data["bots"]
+
+        for bot_data in bots_data:
+            if bot_data["name"] == bot_name:
+                return Bot(**bot_data)
+
+        return bot_data
+
 
     def get_messages(self, bot: Bot, title: str) -> dict[int:Message]:
         target_dir = Path(self.profile_dir, bot.name)
@@ -61,7 +91,8 @@ class Archiver:
     def get_conversation(self, bot: Bot, title: str) -> Conversation:
         messages = self.get_messages(bot, title)
         message_count = len(messages.keys())
-        convo = Conversation(bot, messages, message_count)
+        user = self.get_user()
+        convo = Conversation(user, bot, messages, message_count)
         return convo
 
 
