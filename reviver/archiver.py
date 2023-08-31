e1df62f19a0055b407e2fb2bccf81042e44326f3
mprib <prible@gmail.com>
2023-08-31 17:33:44 -0500

    very basic setting up of database

--- reviver/archiver.py
+++ reviver/archiver.py
@@ -6,71 +6,88 @@ logger = reviver.logger.get(__name__)
 from pathlib import Path
 from reviver.conversation import Conversation, Message
 from reviver.bot import Bot
+from reviver.user import User
 
+import sqlite3
+from reviver import ROOT
+from pathlib import Path
 
 class Archiver:
-    def __init__(self, user_dir: Path) -> None:
-        self.profile_dir = user_dir
+    def __init__(self, reviver_dir: Path) -> None:
+        self.reviver_dir = reviver_dir
+        self.set_db_connection()
 
-    def store_conversation(self, convo: Conversation) -> None:
-        toml_data = {}
-        toml_data["bot"] = asdict(convo.bot)
-
-        messages = get_messages_for_toml(convo)
-        toml_data["messages"] = messages
-        logger.info(messages)
-
-        # conversations are stored under bot subfolder
-
-        target_dir = Path(self.profile_dir, convo.bot.name)
-        target_dir.mkdir(parents=True, exist_ok=True)
-        target_path = Path(target_dir, f"{convo.title}.toml")
-
-        with open(target_path, "w") as f:
-            rtoml.dump(toml_data, f)
-
-    def get_bot(self, bot_name: str, title: str) -> Bot:
-        target_dir = Path(self.profile_dir, bot_name)
-        target_path = Path(target_dir, f"{title}.toml")
-
-        conversation_data = rtoml.load(target_path)
-        bot_data = conversation_data["bot"]
-        bot = Bot(**bot_data)
-
-        return bot
-
-    def get_messages(self, bot: Bot, title: str) -> dict[int:Message]:
-        target_dir = Path(self.profile_dir, bot.name)
-        target_path = Path(target_dir, f"{title}.toml")
-
-        # note that messages are in a table, therefore are a list of dictionaries with identical layouts
-
-        toml_data = rtoml.load(target_path)
-        messages = {}
-        for msg in toml_data["messages"]:
-            role = msg["role"]
-            content = msg["content"]
-            time = msg["time"]
-            index = msg["index"]
-
-            messages[index] = Message(role, content, time)
-
-        logger.info(f"Messages loaded")
-        return messages
-
-    def get_conversation(self, bot: Bot, title: str) -> Conversation:
-        messages = self.get_messages(bot, title)
-        message_count = len(messages.keys())
-        convo = Conversation(bot, messages, message_count)
-        return convo
+        self.create_tables() # won't do anyything if they already exist
+        
+    def set_db_connection(self)->sqlite3.Connection:
+    
+        target_db = Path(self.reviver_dir, "reviver.db")
+        try:
+            self.conn = sqlite3.connect(target_db)
+            logger.info(f"Successfull connection to database at {target_db}")
+        except:
+            logger.info(f"Unable to create or load db from {str(target_db)}")
 
 
-def get_messages_for_toml(convo: Conversation):
-    messages = []
-    logger.info(convo.messages_prompt)
-    for index, msg in convo.messages.items():
-        msg_data = asdict(msg)
-        msg_data["index"] = index
-        messages.append(msg_data)
+    def create_tables(self):
+        cursor = self.conn.cursor()
 
-    return messages
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS user (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            key_path TEXT
+        );
+        """)
+
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS bots (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            user_id INTEGER,
+            FOREIGN KEY(user_id) REFERENCES users(id)
+        );
+        """)
+
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS conversations (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            bot_id INTEGER,
+            user_id INTEGER,
+            FOREIGN KEY(bot_id) REFERENCES bots(id),
+            FOREIGN KEY(user_id) REFERENCES users(id)
+        );
+        """)
+
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS messages (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            content TEXT NOT NULL,
+            time TIMESTAMP NOT NULL,
+            role TEXT NOT NULL,
+            conversation_id INTEGER,
+            FOREIGN KEY(conversation_id) REFERENCES conversations(id)
+        );
+        """)
+
+
+    def store_bot(self,bot:Bot)->None:
+        pass
+    
+    def get_bot(self, bot_id:int)->Bot:
+        pass
+    
+    
+    def store_user(self, user:User)->None:
+        pass
+    
+    def get_user(self)->User:
+        pass
+    
+    
+    def store_conversation(self, convo:Conversation)->None:
+        pass
+    
+    def get__conversation(self,convo_id)->Conversation:
+        pass
