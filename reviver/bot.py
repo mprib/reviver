5e37c47ed75af35b88eff7939d9ecb9f4a668386
mprib <prible@gmail.com>
2023-09-01 09:43:20 -0500

    bots managed by bot gallery

--- reviver/bot.py
+++ reviver/bot.py
@@ -1,9 +1,10 @@
-from dataclasses import dataclass, asdict
+from dataclasses import dataclass, asdict, field
 
 @dataclass
 class Bot:
-    
+     
     name:str # must be unique among all bots in user's profile
+    bot_rank:int # used for ordering bot in list
     model: str
     system_prompt:str = "you are a helpful assistant"
     max_tokens:int = 1000
@@ -12,3 +13,7 @@ class Bot:
     frequency_penalty:float =0
     presence_penalty:float =0
 
+
+@dataclass
+class BotGallery:
+    bots: dict = field(default_factory=dict[str, Bot])
