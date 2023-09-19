e770f00231e48bf54392242e17596d1da0308c94
Mac Prible <prible@gmail.com>
2023-09-19 16:15:16 -0500

    more robust code formatting

--- reviver/gui/markdown_conversion.py
+++ reviver/gui/markdown_conversion.py
@@ -6,6 +6,8 @@ from pygments.lexers import get_lexer_by_name, guess_lexer
 from pygments.formatters import HtmlFormatter
 from reviver import ROOT
 from pathlib import Path
+import reviver.log
+log = reviver.log.get(__name__)
 
 with open(Path(ROOT, "reviver", "gui", "conversation.css")) as f:
     CONTENT_CSS = f.read()
@@ -20,18 +22,21 @@ def style_code_blocks(html)->str:
     for block in soup.find_all('code'):
         
         if block.parent.name == "pre": # full code block
-          language = block.get('class', [None])[0]
-          if language is not None:
-              language = language[9:]  # remove 'language-' prefix
-          code = block.string
+            language = block.get('class', [None])[0]
+            code = block.string
+            if language is not None:
+                language = language[9:]  # remove 'language-' prefix
+                lexer = get_lexer_by_name(language)
+            else:
 
-          if language is None:
-              lexer = guess_lexer(code)
-          else:
-              lexer = get_lexer_by_name(language)
+                try:
+                    lexer = guess_lexer(code)
+                except Exception as e:
+                    log.warn(f"New exception {e}") 
+                    lexer = get_lexer_by_name('python')  # default to python if guess fails 
 
-          highlighted_code = highlight(code, lexer, formatter)
-          block.string.replace_with(BeautifulSoup(highlighted_code, 'html.parser'))
+            highlighted_code = highlight(code, lexer, formatter)
+            block.string.replace_with(BeautifulSoup(highlighted_code, 'html.parser'))
         else:
           # inline code 
           block['style'] = 'font-family: monospace;'
