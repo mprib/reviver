300cf8159d3937f7ce28d960830dcd18b63c6b13
Mac Prible <prible@gmail.com>
2023-09-20 15:11:55 -0500

    reformat query output for tabular display

--- reviver/models_data.py
+++ reviver/models_data.py
@@ -1,88 +1,90 @@
 from reviver.open_router_query_handler import OpenRouterQueryHandler
 from dataclasses import dataclass
+import math
 import reviver.log
 import pandas as pd
 log = reviver.log.get(__name__)
 
 
 @dataclass
-class ModelsData:
-    raw_data:pd.DataFrame
+class ModelSpecSheet:
+    api_key:str
 
     def __post_init__(self):
+        
+        # while i'm not terribly happy with creating an object within another
+        # this is a slightly cleaner way of managing this.        
+
+        query_handler = OpenRouterQueryHandler(self.api_key)
+        
+        self.raw_data = query_handler.get_model_specs()
         self.models = {}
         for index, row in self.raw_data.iterrows():
-            self.models[row["id"]] = ModelData(name=row["id"],
-                                          context_length = row["context_length"],
+            self.models[row["id"]] = ModelSpecs(name=row["id"],
+                                          _context_length = row["context_length"],
                                           pricing_completion=row["pricing_completion"],
                                           pricing_prompt = row["pricing_prompt"],
-                                          pricing_discount = row["pricing_discount"]
+                                          _pricing_discount = row["pricing_discount"]
                                           )
+    
+    
+    @property
+    def in_dollars_per_1k(self)->pd.DataFrame:
+        # going to construct         
 
+        dollars_per_1k = {"Name":[],
+                          "Prompt cost\n(per 1k tokens)":[],
+                          "Completion cost\n(per 1k tokens)":[],
+                          "Context\n(tokens)":[]}
+    
+        for name, model in self.models.items():
+            dollars_per_1k["Name"].append(name)
+            dollars_per_1k["Prompt cost\n(per 1k tokens)"].append(model.prompt_cost_dollars_per_1k)
+            dollars_per_1k["Completion cost\n(per 1k tokens)"].append(model.completion_cost_dollars_per_1k)
+            dollars_per_1k["Context\n(tokens)"].append(model.context_length) 
+
+        return pd.DataFrame(dollars_per_1k)
+   
+    @property
+    def dollars_per_1k_format(self)->dict:
+        
+        format = {"Name":"{}",
+                  "Prompt cost\n(per 1k tokens)":"${:,.6f}",
+                  "Completion cost\n(per 1k tokens)":"${:,.6f}",
+                  "Context\n(tokens)":"{:,}",}
+        
+        return format 
+           
+    @property
+    def in_tokens_per_dollar()->pd.DataFrame:
+        pass
 @dataclass   
-class ModelData:
+class ModelSpecs:
     name:str
-    context_length:int 
+    _context_length:int 
     pricing_completion:float
     pricing_prompt:float
-    pricing_discount:float
+    _pricing_discount:float
     
+    @property
+    def pricing_discount(self):
+        if math.isnan(self._pricing_discount):
+            return 0
+    
+        else:
+            return self._pricing_discount
+             
     @property
     def completion_cost_dollars_per_1k(self):
-        return self.pricing_completion *1000
+        value = round(self.pricing_completion*(1-self.pricing_discount)*1000,6)
+        return "${:,.6f}".format(value)
     
-    @property
-    def completion_cost_tokens_per_dollar(self):
-        return (1/self.pricing_completion)
-        
-
     @property
     def prompt_cost_dollars_per_1k(self):
-        return self.pricing_prompt *1000
+        value = round(self.pricing_prompt*(1-self.pricing_discount) *1000, 6)
+        return "${:,.6f}".format(value)
     
     @property
-    def prompt_cost_tokens_per_dollar(self):
-        return (1/self.pricing_prompt)
-
-
-if __name__ == "__main__":
-    from PySide6.QtWidgets import QApplication, QTableView
-    from PySide6.QtCore import Qt
-    from PySide6.QtGui import QStandardItemModel, QStandardItem
-    import pandas as pd
-    from io import StringIO
-    import sys
-    from dotenv import load_dotenv
-    from os import getenv
-    from pathlib import Path
-
-    archive_dir = Path(Path.home(), "reviver")
-    env_location = Path(archive_dir,".env")
-    load_dotenv(dotenv_path=env_location)
-    key = getenv("OPEN_ROUTER_API_KEY")
-
-    query_handler = OpenRouterQueryHandler(key)
-    
-    key_usage = query_handler.get_key_usage()
-    
-    print(key_usage)
-    model_specs =  query_handler.get_model_specs()
-
-    log.info(model_specs.to_csv())
-
-    model_specs
-    app = QApplication(sys.argv)
-
-    model = QStandardItemModel()
-
-    model.setHorizontalHeaderLabels(model_specs.columns)
-    for row in model_specs.values:
-        items = [QStandardItem(str(i)) for i in row]
-        model.appendRow(items)
-
-    view = QTableView()
-    view.setModel(model)
-    view.show()
-
-    sys.exit(app.exec())
-# %%
+    def context_length(self):
+        
+        return "{:,}".format(self._context_length)
