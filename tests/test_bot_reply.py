164cebeba974240100c4f4299bfb7f889e60217f
Mac Prible <prible@gmail.com>
2023-09-21 16:50:56 -0500

    simplified conversation and message

--- tests/test_bot_reply.py
+++ tests/test_bot_reply.py
@@ -7,13 +7,10 @@ Need to make sure i don't backup to git and that I lock down the
 max spend on the openAI key
 
 """
-from reviver.user import User
 from reviver.bot import Bot
-from reviver.conversation import Conversation, Message
-from reviver import ROOT
-from pathlib import Path
+from reviver.conversation import Conversation
+from reviver.message import Message
 from queue import Queue
-import sys
 import reviver.log
 log = reviver.log.get(__name__)
 
@@ -22,21 +19,16 @@ def test_bot_reply():
     Note that this test requires an active internet connection and working keys
     Keys are stored in ROOT/keys.toml which should *not* be version controlled
     """
-    test_key_toml = Path(ROOT, "keys.toml")
-    user = User("test", dot_env_loc=test_key_toml)
     # model = "meta-llama/llama-2-70b-chat"
     model = "openai/gpt-3.5-turbo-0301"
     bot = Bot(_id=1, name = "test_bot", model=model,rank=1, system_prompt="You will only reply with the word:HELLO WORLD. It should be spelled in all capitals with no other punctuation. User will say 'proceed' and then you will responsd with 'HELLO WORLD'.")
 
     convo_id = 1
-    convo = Conversation(_id=convo_id, user=user, bot=bot)
+    convo = Conversation(_id=convo_id, bot=bot)
 
     assert(convo.message_count==1) # system prompt
 
-    message1 = Message(conversation_id=convo_id, 
-                       role="user", 
-                       content= "proceed"
-                       )  
+    message1 = Message( role="user", content= "proceed")  
 
     log.info("Add user generated message to conversation")
     convo.add_message(message1)
