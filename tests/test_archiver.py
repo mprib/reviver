ab716524d2a3e408238632fe6763a6f408f61e9e
Mac Prible <prible@gmail.com>
2023-09-25 18:59:11 -0500

    refactor to allow archive to store and retrieve conversation

--- tests/test_archiver.py
+++ tests/test_archiver.py
@@ -55,59 +55,33 @@ def test_bot_gallery_store_retrieve():
     bot_gallery_copy = archive.load_bot_gallery()
     assert(bot_gallery==bot_gallery_copy)
 
-    
-def test_message_store_retrieve():
-    msg = Message(role="user",content= "hello world!")
-
-    archive = get_new_test_archiver()
-    archive.store_message(msg)
-    msg_copy = archive.get_message(1,1)
-    assert(msg == msg_copy)
-    
-    
-def test_messages_store_retrieve():
-    msg1 = Message(role="user",content= "hello world!")
-    msg2 = Message(role="assistant",content= "sup?")
-
-    messages = {msg1.position:msg1, msg2.position:msg2}
-
-    archive = get_new_test_archiver()
-    # archive.store_messages(messages)
-    for position, msg in messages.items():
-        archive.store_message(msg)
-    messages_copy = archive.get_messages(conversation_id=1)
-
-    assert(messages[1] == messages_copy[1])
-    assert(messages[2] == messages_copy[2])
 
 def test_convo_store_retrieve():
     
-    msg1 = Message(conversation_id=1,position=1, role="user",content= "hello world!")
-    msg2 = Message(conversation_id=1,position=2, role="assistant",content= "sup?")
+    msg1 = Message(role="user",content= "hello world!")
+    msg2 = Message(role="assistant",content= "sup?")
 
-    bot_1 = Bot(1,"test_bot", model="llama_70b", rank=1)
-    bot_2 = Bot(2,"test_bot", model="llama_70b", rank=2)
+    bot_1 = Bot("test_bot", model="llama_70b", rank=1)
+    bot_2 = Bot("test_bot2", model="llama_70b", rank=2)
 
     bot_gallery = BotGallery()
     bot_gallery.add_bot(bot_1)
     bot_gallery.add_bot(bot_2)
     
-    convo = Conversation(1, bot=bot_1)
+    convo = Conversation(bot=bot_1, title="test")
 
     convo.add_message(msg1)
     convo.add_message(msg2)
 
     archive = get_new_test_archiver()
     archive.store_conversation(convo)
-    convo_copy = archive.get_conversation(convo_id=1, bot_gallery=bot_gallery) 
-    logger.info("Confirm that saved and reloaded convo is same as original")
-    assert(convo==convo_copy)
+
+    convo_copy = archive.load_conversation(convo_title="test", bot_gallery=bot_gallery)
+    assert(convo_copy==convo)
 
 def get_new_test_archiver()->Archive:
     test_dir = Path(ROOT, "tests", "working_delete")
     delete_directory_contents(test_dir)
-    db_path = Path(test_dir,"reviver.db")
-    assert(not db_path.exists())
     archive = Archive(test_dir)
     return archive
     
@@ -117,7 +91,5 @@ if __name__ == "__main__":
     test_archive_init()
     test_bot_store_retrieve()
     test_bot_gallery_store_retrieve()
-    # test_message_store_retrieve()
-    # test_messages_store_retrieve()
-    # test_convo_store_retrieve()
+    test_convo_store_retrieve()
 
