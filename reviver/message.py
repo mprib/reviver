42bf7dbf51715daa55bca85c6b6f75bde747b944
Mac Prible <prible@gmail.com>
2023-10-03 19:59:45 -0500

    strip out styling

--- reviver/message.py
+++ reviver/message.py
@@ -1,12 +1,7 @@
-from bs4 import BeautifulSoup
-from pygments import highlight
-from pygments.lexers import get_lexer_by_name, guess_lexer
-from pygments.formatters import HtmlFormatter
 from reviver import ROOT
 from pathlib import Path
 from dataclasses import dataclass
 from datetime import datetime
-import markdown
 import reviver.log
 
 log = reviver.log.get(__name__)
@@ -27,19 +22,6 @@ class Message:
     def __post_init__(self):
         if self.time is None:
             self.time = datetime.now()
-
-    @property
-    def backtic_complete_content(self) -> str:
-        """
-        Used to make sure that the stylized html will render python correctly when it is in the middle
-        of being drafted by the bot.
-        """
-        # check if the content has an odd number of triple backticks
-        if self.content.count("```") % 2 != 0:
-            # if so, append a set of triple backticks to the end
-            return self.content + "\n```"
-        else:
-            return self.content
 
     @property
     def _id(self):
@@ -63,55 +45,6 @@ class Message:
         t = datetime.strptime(self.time, format)
         return t
 
-    def as_html(self):
-        # html_version = markdown.markdown(self.content, extensions=['fenced_code'])
-        html_version = markdown.markdown(
-            self.backtic_complete_content, extensions=["fenced_code"]
-        )
-        return html_version
-
-    def as_styled_html(self):
-        styled_html = f"""<div id='{self._id}' class='message {self.role}'>
-                            {"<p>SYSTEM PROMPT</p>" if self.role == "system" else ""}
-                            {self.as_html()}
-                        </div>
-                        """
-        styled_html = style_code_blocks(styled_html)
-
-        return styled_html
 
 
-with open(Path(ROOT, "reviver", "gui", "conversation.css")) as f:
-    CONTENT_CSS = f.read()
 
-
-def style_code_blocks(html) -> str:
-    soup = BeautifulSoup(html, "html.parser")
-
-    formatter = HtmlFormatter(style="monokai", full=False, cssstyles="overflow:auto;")
-
-    code_css = formatter.get_style_defs(".highlight")
-
-    for block in soup.find_all("code"):
-        if block.parent.name == "pre":  # full code block
-            language = block.get("class", [None])[0]
-            code = block.string
-            if language is not None:
-                language = language[9:]  # remove 'language-' prefix
-                lexer = get_lexer_by_name(language)
-            else:
-                try:
-                    lexer = guess_lexer(code)
-                except Exception as e:
-                    log.warn(f"New exception {e}")
-                    lexer = get_lexer_by_name(
-                        "python"
-                    )  # default to python if guess fails
-
-            highlighted_code = highlight(code, lexer, formatter)
-            block.string.replace_with(BeautifulSoup(highlighted_code, "html.parser"))
-        else:
-            # inline code
-            block["style"] = "font-family: monospace;"
-
-    return f"<style>{code_css}</style>" + str(soup)
