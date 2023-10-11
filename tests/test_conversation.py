ebd5f5373c2a6951505d7a66e88328e085852a55
Mac Prible <prible@gmail.com>
2023-10-11 14:31:40 -0500

    new convo manager features tested

--- tests/test_conversation.py
+++ tests/test_conversation.py
@@ -1,6 +1,7 @@
 import reviver.log
 
 from reviver.conversation import Conversation
+from reviver.conversation_manager import ConversationManager
 from reviver.message import Message
 from reviver.bot import Bot
 logger = reviver.log.get(__name__)
@@ -45,6 +46,63 @@ def test_token_size():
     logger.info(f"conversation size is {convo.token_size}")
 
 
+def test_conversation_list_order():
+    convo_manager = ConversationManager()
+
+    bot = Bot("test_bot", model="test", rank=1)
+    for i in range(5):
+        
+        convo_manager.new_active_conversation(bot)
+        convo_manager.active_conversation.add_user_message(f"Hi bot! This is conversation {i}")
+        title = convo_manager.active_conversation.title
+        convo_manager.rename_conversation(title, f"Convo {i}")
+    
+
+    logger.info("Title order")
+    convo_order = []
+    for title in convo_manager.get_conversation_list():
+        logger.info(title)
+        convo_order.append(int(title[-1]))
+    
+    assert(convo_order == [4,3,2,1,0])
+    
+    convo_manager.set_active_conversation("Convo 2")
+    convo_manager.active_conversation.add_user_message("This is a new message")
+
+    convo_order = []
+    for title in convo_manager.get_conversation_list():
+        logger.info(title)
+        convo_order.append(int(title[-1]))
+    assert(convo_order == [2,4,3,1,0])
+
+    logger.info("Title order")
+    for title in convo_manager.get_conversation_list():
+        logger.info(title)
+
+   
+def test_conversation_start():
+    """
+    starts a couple of conversations and uses the active convo reference to make some changes
+    makes sure the conversations are being created and pointed to correctly
+    """
+    convo_manager = ConversationManager()
+
+    bot = Bot("test_bot", model="test", rank=1)
+    convo_manager.new_active_conversation(bot)
+    title = convo_manager.active_conversation.title
+    convo_manager.rename_conversation(title, "Convo 1")
+    
+    convo_manager.new_active_conversation(bot)
+    title = convo_manager.active_conversation.title
+    convo_manager.rename_conversation(title, "Convo 2")
+
+    assert(convo_manager.conversations['Convo 1'] != convo_manager.conversations['Convo 2'])
+
+    
+
+
 if __name__ == "__main__":
     test_conversation_creation()
     test_token_size()
+    test_conversation_list_order()
+    test_conversation_start()
